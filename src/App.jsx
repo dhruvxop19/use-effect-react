1e6c9d792d11b63fec20f5b4177165d9f4c94bf8
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-21 13:54:32 +0530

    lists and keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import React from 'react';
 
-const Card = ({ children }) => {
-    return (
-        <div style={{
-            border: '1px solid #ccc',
-            borderRadius: '5px',
-            padding: '20px',
-            margin: '10px',
-            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
-        }}>
-            {children}
-        </div>
-    );
+const App = () => {
+
+  return (
+<div>
+{[
+  <Todo key = {1} title = {"go to gym"} done = {false}/>,
+  <Todo key = {2} title = {"Eat food"} done = {false}/>
+]}
+
+</div>
+  );
+
 };
 
-const App = () => {
-    return (
-        <div>
-            <Card>
-                <h2>Card Title</h2>
-                <p>This is some content inside the card.</p>
-            </Card>
-            <Card>
-                <h2>Another Card</h2>
-                <p>This card has different content!</p>
-            </Card>
-        </div>
-    );
-};
+
+function Todo ({title,done}){
+  return <div>
+    {title} - { done ?  "done!" : "notdone!"}
+  </div>
+}
 
 export default App;
