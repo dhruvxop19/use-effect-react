30a38e0dcecaa08083a1e26c5506074d77067a79
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-21 11:59:07 +0530

    children done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-function App (){
+import React from 'react';
 
-  return <div style={{display:"flex"}}>
-    <Card innerContent={<div style={{color:"green"}}>What do you want to post <br /> <br />
-    <input type={"text"}/>
-    </div>}/>
+const Card = ({ children }) => {
+    return (
+        <div style={{
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            padding: '20px',
+            margin: '10px',
+            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
+        }}>
+            {children}
+        </div>
+    );
+};
 
-<Card innerContent={"hi there"}/>
+const App = () => {
+    return (
+        <div>
+            <Card>
+                <h2>Card Title</h2>
+                <p>This is some content inside the card.</p>
+            </Card>
+            <Card>
+                <h2>Another Card</h2>
+                <p>This card has different content!</p>
+            </Card>
+        </div>
+    );
+};
 
-  </div>
-}
-
-
-function Card ({innerContent}){
-
-  return <div style={{ background : "black", 
-    borderRadius : 10 , color: "white" , padding : 10, 
-    margin:10
-  }}>
-    {innerContent}
-
-  </div>
-}
-
-export default App
+export default App;
